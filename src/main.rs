48269fc5a149ae07c0c996f05b2ac396663fbdfa
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-02-22 13:36:13 +0300

    Initial commit with the code from near-indexer-for-explorer
    306 compilation errors :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod models;
+mod db_adapters;
+
 use bigdecimal::BigDecimal;
 use dotenv::dotenv;
 use futures::StreamExt;
@@ -11,34 +14,8 @@ use std::convert::TryFrom;
 use std::env;
 use std::str::FromStr;
 use tracing_subscriber::EnvFilter;
+use crate::models::Block;
 
-
-#[derive(Debug, FromRow)]
-pub struct Block {
-    pub block_height: BigDecimal,
-    pub block_hash: String,
-    pub prev_block_hash: String,
-    pub block_timestamp: BigDecimal,
-    pub total_supply: BigDecimal,
-    pub gas_price: BigDecimal,
-    pub author_account_id: String,
-}
-
-impl From<&near_indexer_primitives::views::BlockView> for Block {
-    fn from(block_view: &near_indexer_primitives::views::BlockView) -> Self {
-        Self {
-            block_height: block_view.header.height.into(),
-            block_hash: block_view.header.hash.to_string(),
-            prev_block_hash: block_view.header.prev_hash.to_string(),
-            block_timestamp: block_view.header.timestamp.into(),
-            total_supply: BigDecimal::from_str(block_view.header.total_supply.to_string().as_str())
-                .expect("`total_supply` expected to be u128"),
-            gas_price: BigDecimal::from_str(block_view.header.gas_price.to_string().as_str())
-                .expect("`gas_price` expected to be u128"),
-            author_account_id: block_view.author.to_string(),
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
